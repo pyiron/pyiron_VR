1113b7c9916aa29fdb960fcb46b66593516bd9fa
wuseling <p.neugebauer@mpie.de>
2017-08-23 15:20:50 +0200

    Added a new script Mode, which defines the class Mode, which holds the different properties of a mode.

---
+++ Assets/Scripts/Mode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+public class Mode
+{
+    // create the variables, which hold the data of the properties of an element
+    public readonly string name;
+    public readonly bool playerCanMoveAtoms;
+    public readonly bool showTemp;
+    public readonly bool showRelaxation;
+    public readonly bool showInfo;
+    public readonly bool canDuplicate;
+
+    // create an object which holds the infos of a mode
+    public Mode(string m_name, bool m_playerCanMoveAtoms = false, bool m_showTemp = false, bool m_showRelaxation = false,
+        bool m_showInfo = false, bool m_canDuplicate = false)
+    {
+        name = m_name;
+        playerCanMoveAtoms = m_playerCanMoveAtoms;
+        showTemp = m_showTemp;
+        showRelaxation = m_showRelaxation;
+        showInfo = m_showInfo;
+        canDuplicate = m_canDuplicate;
+    }
+}
