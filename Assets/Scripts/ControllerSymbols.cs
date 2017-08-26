84864b506505dac2d9116f650325b9ff8886d141
wuseling <p.neugebauer@mpie.de>
2017-08-26 13:17:09 +0200

    Imported some of the new symbols for the animation, shown on the touchpad, and a script which should handle them in the future.

---
+++ Assets/Scripts/ControllerSymbols.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerSymbols : MonoBehaviour {
+    private GameObject[] AnimSymbols = GameObject[7];
+
+	// Use this for initialization
+	void Start () {
+		//Resources
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetSymbol(float symbol)
+    {
+
+    }
+}
