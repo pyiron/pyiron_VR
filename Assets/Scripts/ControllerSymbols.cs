14c73a800e61ba68c4c99bb3c5a0382aef4fbadd
wuseling <p.neugebauer@mpie.de>
2017-08-26 17:45:12 +0200

    Renamed the symbols for the animation controll and wrote an uncomplete way how to activate/deactivate the symbols.

--- Assets/Scripts/ControllerSymbols.cs
+++ Assets/Scripts/ControllerSymbols.cs
@@ -6,18 +6,20 @@ public class ControllerSymbols : MonoBehaviour {
     // get the reference to the programm which handles the execution of python
     public PythonExecuter PE;
 
-    private GameObject[] AnimSymbols = new GameObject[8];
+    private GameObject[] AnimSymbols = new GameObject[10];
 
     private static readonly Dictionary<string, Symbol> controllerSymbols = new Dictionary<string, Symbol>
     {
-        {"Triangle 0", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
-        {"Pause 0", new Symbol(new Vector3(0.000f, 0.002f, -0.049f), new Vector3(-90, 0, 0), 0.005f, new Color(1, 1, 1, 1)) },
-        {"Triangle 1", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
-        {"Triangle 2", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
-        {"Triangle 3", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
-        {"Triangle 4", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
-        {"Triangle 5", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
-        {"Triangle 6", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) }
+        {"Triangle 0", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1), false) },
+        {"Pause 0", new Symbol(new Vector3(0.000f, 0.00f, -0.049f), new Vector3(-90, 0, 0), 0.005f, new Color(1, 1, 1, 1), true) },
+        {"FastForward 0", new Symbol(new Vector3(0.012f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1), true, 5) },
+        {"FastForward 1", new Symbol(new Vector3(-0.008f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1), true, 0) },
+        {"FrameForward 0", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1), false, -2) },
+        {"FrameForward 1", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1), false, -3) },
+        {"Triangle 2", new Symbol(new Vector3(0.016f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.003f, new Color(1, 1, 1, 1), true, 4) },
+        {"Triangle 1", new Symbol(new Vector3(-0.012f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.003f, new Color(1, 1, 1, 1), true, 1) },
+        {"TimelapsForward 0", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1), true, 2) },
+        {"TimelapsForward 1", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1), true, 3) }
     };
 
     // Use this for initialization
@@ -44,13 +46,42 @@ public class ControllerSymbols : MonoBehaviour {
     public void SetSymbol()
     {
         foreach (GameObject AnimSymbol in AnimSymbols)
-            if (PE.pythonRunsAnim && AnimSymbol.name.Contains("Pause"))
+        {
+
+            /*if (PE.pythonRunsAnim)
+                if (AnimSymbol.name.Contains("Pause"))
+                    AnimSymbol.SetActive(true);
+                else if (PE.pythonsAnimSpeed == 0)
+                    if (AnimSymbol.name.Contains("Triangle 1"))
+                        AnimSymbol.SetActive(true);
+                    else;
+                else if (PE.pythonsAnimSpeed == 1)
+                    if (AnimSymbol.name.Contains("Fast Forward 1") || AnimSymbol.name.Contains("TimelapsForward 1"))
+                        AnimSymbol.SetActive(true);
+                    else;
+                else if (PE.pythonsAnimSpeed == 4)
+                    if (AnimSymbol.name.Contains("Fast Forward 0") || AnimSymbol.name.Contains("TimelapsForward 0"))
+                        AnimSymbol.SetActive(true);
+                    else;
+                else if (PE.pythonsAnimSpeed == 5)
+                    if (AnimSymbol.name.Contains("Triangle 2"))
+                        AnimSymbol.SetActive(true);
+                    else;
+                else
+                    AnimSymbol.SetActive(false);
+            else
+                if (AnimSymbol.name.Contains("Triangle 0"))
+                    AnimSymbol.SetActive(true);
+                else
+                    AnimSymbol.SetActive(false);*/
+
+
+            /*else if (PE.pythonRunsAnim && PE.pythonsAnimSpeed == 3 && AnimSymbol.name.Contains("Triangle 2"))
                 AnimSymbol.SetActive(true);
-            else if (!PE.pythonRunsAnim && AnimSymbol.name.Contains("Triangle"))
-                AnimSymbol.SetActive(true);
-            else if (PE.pythonRunsAnim && PE.pythonsAnimSpeed == 4 && AnimSymbol.name.Contains("Fast Forward"))
+            else if (PE.pythonRunsAnim && PE.pythonsAnimSpeed == 2 && AnimSymbol.name.Contains("Triangle 1"))
                 AnimSymbol.SetActive(true);
             else
-                AnimSymbol.SetActive(false);
+                AnimSymbol.SetActive(false);*/
+        }
     }
 }
--- Assets/Scripts/Symbol.cs
+++ Assets/Scripts/Symbol.cs
@@ -9,13 +9,17 @@ public class Symbol
     public readonly Vector3 m_rotation;
     public readonly float m_size;
     public readonly Color m_color;
+    public readonly bool m_showWhenAnimRuns;
+    public readonly int m_animSpeed;
 
     // create an object which holds the infos of an atom, without a special size and colour
-    public Symbol(Vector3 position, Vector3 rotation, float size, Color color)
+    public Symbol(Vector3 position, Vector3 rotation, float size, Color color, bool showWhenAnimRuns, int animSpeed=-1)
     {
         m_position = position;
         m_rotation = rotation;
         m_size = size;
         m_color = color;
+        m_showWhenAnimRuns = showWhenAnimRuns;
+        m_animSpeed = animSpeed;
     }
 }
