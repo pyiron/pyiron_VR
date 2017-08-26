f477a22c2d31d79d7fa55a3129062ba4c5d60d80
wuseling <p.neugebauer@mpie.de>
2017-08-26 15:49:12 +0200

    The program now shows the start and stop symbol for the animation, but it has the wrong rotation on one controller.

--- Assets/Scripts/ControllerSymbols.cs
+++ Assets/Scripts/ControllerSymbols.cs
@@ -6,22 +6,47 @@ public class ControllerSymbols : MonoBehaviour {
     // get the reference to the programm which handles the execution of python
     public PythonExecuter PE;
 
-    private GameObject[] AnimSymbols = new GameObject[7];
+    private GameObject[] AnimSymbols = new GameObject[8];
 
-	// Use this for initialization
-	void Start () {
-        AnimSymbols = Resources.LoadAll("ControllerSymbols", typeof(GameObject)) as GameObject[];
+    private static readonly Dictionary<string, Symbol> controllerSymbols = new Dictionary<string, Symbol>
+    {
+        {"Triangle 0", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
+        {"Pause 0", new Symbol(new Vector3(0.000f, 0.002f, -0.049f), new Vector3(-90, 0, 0), 0.005f, new Color(1, 1, 1, 1)) },
+        {"Triangle 1", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
+        {"Triangle 2", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
+        {"Triangle 3", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
+        {"Triangle 4", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
+        {"Triangle 5", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) },
+        {"Triangle 6", new Symbol(new Vector3(0.002f, 0.005f, -0.049f), new Vector3(-90, 0 -90), 0.005f, new Color(1, 1, 1, 1)) }
+    };
+
+    // Use this for initialization
+    void Start () {
+        //AnimSymbols = Resources.LoadAll("ControllerSymbols", typeof(GameObject)) as GameObject[];
+        int objectCounter = 0;
+        foreach (string symbolKey in controllerSymbols.Keys)
+        {
+            AnimSymbols[objectCounter] = Instantiate(Resources.Load("ControllerSymbols/" + symbolKey.Split()[0]) as GameObject);
+            AnimSymbols[objectCounter].transform.parent = transform;
+            AnimSymbols[objectCounter].transform.eulerAngles = controllerSymbols[symbolKey].m_rotation;
+            AnimSymbols[objectCounter].transform.localPosition = controllerSymbols[symbolKey].m_position;
+            AnimSymbols[objectCounter].transform.localScale = Vector3.one * controllerSymbols[symbolKey].m_size;
+            objectCounter += 1;
+            print("init " + AnimSymbols[objectCounter - 1]);
+        }
+        SetSymbol();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
 
     public void SetSymbol()
     {
-        /*foreach (GameObject AnimSymbol in AnimSymbols)
+        foreach (GameObject AnimSymbol in AnimSymbols)
         {
+            print(AnimSymbol);
             if (PE.pythonRunsAnim && AnimSymbol.name.Contains("Pause"))
                 AnimSymbol.SetActive(true);
             else if (!PE.pythonRunsAnim && AnimSymbol.name.Contains("Triangle"))
@@ -30,6 +55,6 @@ public class ControllerSymbols : MonoBehaviour {
                 AnimSymbol.SetActive(true);
             else
                 AnimSymbol.SetActive(false);
-        }*/
+        }
     }
 }
---
+++ Assets/Scripts/Symbol.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class Symbol
+{
+    // create the variables, which hold the data of the properties of an element
+    public readonly GameObject m_object;
+    public readonly Vector3 m_position;
+    public readonly Vector3 m_rotation;
+    public readonly float m_size;
+    public readonly Color m_color;
+
+    // create an object which holds the infos of an atom, without a special size and colour
+    public Symbol(Vector3 position, Vector3 rotation, float size, Color color)
+    {
+        m_position = position;
+        m_rotation = rotation;
+        m_size = size;
+        m_color = color;
+    }
+}
