c1b3e20def213b6011fb84b866d59c038ce369ce
wuseling <p.neugebauer@mpie.de>
2017-09-05 12:07:22 +0200

    The animation of the thermometer will now display the temperature. Experienced a strange bugs when trying to set the structure to another spawn place.

--- Assets/Scripts/Tools/Thermometer.cs
+++ Assets/Scripts/Tools/Thermometer.cs
@@ -7,14 +7,20 @@ public class Thermometer : MonoBehaviour {
     // the Settings of the program
     private ProgramSettings Settings;
 
+    // the reference to the animationController of the thermometer
+    private Animator anim;
     // the text on the thermometer how high the temperature currently is
     private TextMesh ThermometerText;
     // the size the text on the thermometer telling the temperature should have
     private float TextSize = 0.4f;
+    // the max temperature you can set with the thermometer and when the thermometer won't show any higher temperatures
+    private float maxTemperature = 10000;
 
     private void Awake()
     {
-
+        // get the reference to the animationController of the thermometer
+        anim = gameObject.GetComponent<Animator>();
+        // get the Settings of the program
         Settings = GameObject.Find("Settings").GetComponent<ProgramSettings>();
         // get the reference to the TextMesh of the temperature
         ThermometerText = GetComponentInChildren<TextMesh>();
@@ -35,5 +41,8 @@ public class Thermometer : MonoBehaviour {
     public void UpdateTemperature()
     {
         ThermometerText.text = Settings.temperature.ToString();
+        print(Settings.temperature / maxTemperature);
+        anim.SetFloat("Temperature", Settings.temperature / maxTemperature);
+        print("get " + anim.GetFloat("Temperature"));
     }
 }
