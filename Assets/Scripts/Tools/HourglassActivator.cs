197348e555ec1ea3e1b63f5d44af80bc5543cb00
wuseling <p.neugebauer@mpie.de>
2017-09-13 19:47:08 +0200

    Added comments to the HourglassActivator Script.

--- Assets/Scripts/Tools/HourglassActivator.cs
+++ Assets/Scripts/Tools/HourglassActivator.cs
@@ -2,34 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Component of HourglassRotator
 public class HourglassActivator : MonoBehaviour {
+    // the reference to the Hourglass
     private GameObject Hourglass;
-
+    // the hourglass will just be activated, if this timer is <= 0
     private float timer;
-
-    private float timeUntilHourglassGetsShown = 0.4f;
+    // the time, how long the structure should be at least loading until activating the hourglass
+    private float timeUntilHourglassGetsShown = 0.3f;
 
     private void Awake()
     {
+        // get the reference to the Hourglass
         foreach (Transform Child in transform)
             Hourglass = Child.gameObject;
     }
 
-    // Use this for initialization
     void Start () {
+        // set the timer
         timer = timeUntilHourglassGetsShown;
     }
 	
-	// Update is called once per frame
 	void Update () {
+        // check if the hourglass is deactivated while it should be activated or vice versa
         if ((PythonExecuter.outgoingChanges != PythonExecuter.incomingChanges) != Hourglass.activeSelf)
+            // check if the hourglass just has to be deactivated or if the timer is already <= 0
             if (Hourglass.activeSelf || timer <= 0)
             {
+                // activate or deactivate the hourglass
                 Hourglass.GetComponent<Hourglass>().ActivateHourglass(!Hourglass.activeSelf);
-                if (Hourglass.activeSelf)
+                // set the timer for the next time, if the hourglass got deactivated
+                if (!Hourglass.activeSelf)
                     timer = timeUntilHourglassGetsShown;
             }
             else
+                // decrease the time passed after the last update from the timer
                 timer -= Time.deltaTime;
 
     }
