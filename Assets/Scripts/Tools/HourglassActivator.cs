feff8a405379bcd6668519254e0b78515ce7d0e9
wuseling <p.neugebauer@mpie.de>
2017-09-13 19:34:58 +0200

    The hourglass will now just show itself after a short period of time.

--- Assets/Scripts/Tools/HourglassActivator.cs
+++ Assets/Scripts/Tools/HourglassActivator.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class HourglassActivator : MonoBehaviour {
     private GameObject Hourglass;
 
+    private float timer;
+
+    private float timeUntilHourglassGetsShown = 0.4f;
+
     private void Awake()
     {
         foreach (Transform Child in transform)
@@ -13,12 +17,20 @@ public class HourglassActivator : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
+        timer = timeUntilHourglassGetsShown;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if ((PythonExecuter.outgoingChanges != PythonExecuter.incomingChanges) != Hourglass.activeSelf)
-            Hourglass.GetComponent<Hourglass>().ActivateHourglass(!Hourglass.activeSelf);
-	}
+            if (Hourglass.activeSelf || timer <= 0)
+            {
+                Hourglass.GetComponent<Hourglass>().ActivateHourglass(!Hourglass.activeSelf);
+                if (Hourglass.activeSelf)
+                    timer = timeUntilHourglassGetsShown;
+            }
+            else
+                timer -= Time.deltaTime;
+
+    }
 }
