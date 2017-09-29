2d741385f1bbd632739e892e3675b4e4c5cf7575
wuseling <p.neugebauer@mpie.de>
2017-09-29 21:08:45 +0200

    Added some more documentation. Moved the old backup to outside the project.

--- Backup/Scripts13(animLeftCtrlBuggy+halfwayToResizeWCtrls)/AtomID.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// Component of the atom prefab, so of each instance of an atom
-public class AtomID : MonoBehaviour {
-    // stores the unique ID each atom gets when it is created
-    public int ID;
-}
