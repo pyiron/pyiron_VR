23869407d9d3d8023cb34ee3d2532676438ff432
Neugebauer, Philip (wuseling) <p.neugebauer@googlemail.com>
2018-09-21 13:46:30 +0200

    Fixed error that the PythonExecuter.cs throws an error when detecting a script in an folder.

--- Assets/Scripts/Input/PythonExecuter.cs
+++ Assets/Scripts/Input/PythonExecuter.cs
@@ -156,10 +156,11 @@ public class PythonExecuter : MonoBehaviour {
         {
             StructureMenuController.inst.AddOption(OptionType.Job, inp.Substring(6));
         }
-        /*else if (splittedData[0] == "files")
+        else if (splittedData[0] == "files")
         {
-            StructureMenuController.inst.AddOption(OptionType.Script, inp.Substring(6));
-        }*/
+            print("Files detected, they are not needed, but do no harm.")
+            //StructureMenuController.inst.AddOption(OptionType.Script, inp.Substring(6));
+        }
         else if (splittedData[0] == "path")
         {
             if (StructureMenuController.currPath != splittedData[1])
