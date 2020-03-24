2cdf8f1d230b4a2ef403f999d5ce342d69f38540
Wuselwog <30385654+Wuselwog@users.noreply.github.com>
2020-03-24 12:53:30 +0100

    Refactored/Cleaned up some code.

--- Assets/Scripts/Modes/ModeController.cs
+++ Assets/Scripts/Modes/ModeController.cs
@@ -22,14 +22,17 @@ public class ModeController : MonoBehaviour
     // the dictionary which defines what properties each mode has
     // attention: the trashcan will just be shown if m_playerCanMoveAtoms is true, even if m_showTrashcan is true
     // attention: the mode will just be accessible, if m_playerCanMoveAtoms, m_showInfo or m_canDuplicate is true
-    internal static List<Mode> modes = new List<Mode>() {
-        new Mode(mode:Modes.Network, hideAtoms: true, showNetwork: true),
-        new Mode(mode:Modes.Explorer, hideAtoms: true, showExplorer: true),
-        new Mode(mode:Modes.Simulation, playerCanMoveAtoms:true, playerCanResizeAtoms:true, showTemp:true, showTrashcan:true),
-        new Mode(mode:Modes.Minimize, playerCanMoveAtoms:true, playerCanResizeAtoms:true, showRelaxation:true, showTrashcan:true),
+    internal static List<Mode> modes = new List<Mode>
+    {
+        new Mode(mode:Modes.Network, hideAtoms: true),
+        new Mode(mode:Modes.Explorer, hideAtoms: true),
+        new Mode(mode:Modes.Simulation, playerCanMoveAtoms:true, playerCanResizeAtoms:true, showTemp:true,
+            showTrashcan:true),
+        new Mode(mode:Modes.Minimize, playerCanMoveAtoms:true, playerCanResizeAtoms:true, showRelaxation:true,
+            showTrashcan:true),
         new Mode(mode:Modes.Animate),
-        new Mode(mode:Modes.StructureBuilder, showPeriodicSystem:true),
-        new Mode(mode:Modes.Menu, showModes:true)
+        new Mode(mode:Modes.StructureBuilder),
+        new Mode(mode:Modes.Menu)
         //new Mode(mode:Modes.Info, showInfo:true)
     };
 
@@ -61,7 +64,8 @@ public class ModeController : MonoBehaviour
     // change the mode. This includes updating scene, e.g. (de)activating the thermometer or UI
     public void SetMode(Modes newMode)
     {
-        if (currentMode.mode != Modes.None && !currentMode.showExplorer && !currentMode.showNetwork)
+        if (currentMode.mode != Modes.None && currentMode.mode != Modes.Explorer &&
+            currentMode.mode != Modes.Network)
             // stop the currently running animation
             AnimationController.RunAnim(false);
         currentMode = modes[(int)newMode];
@@ -81,7 +85,7 @@ public class ModeController : MonoBehaviour
         // deactivate the structure if it shouldn't be shown, else activate it
         StructureData.inst.gameObject.SetActive(!modes[(int)currentMode.mode].hideAtoms);
 
-        if (currentMode.showExplorer)
+        if (currentMode.mode == Modes.Explorer)
         {
             ProgramSettings.inst.ResetScene();
         }
@@ -114,11 +118,11 @@ public class ModeController : MonoBehaviour
     // determine which panels and buttons should be activated/deactivated
     private void UpdateMenu()
     {
-        NetworkMenuController.inst.SetState(currentMode.showNetwork);
-        ExplorerMenuController.inst.SetState(currentMode.showExplorer);
+        NetworkMenuController.inst.SetState(currentMode.mode == Modes.Network);
+        ExplorerMenuController.inst.SetState(currentMode.mode == Modes.Explorer);
         TemperatureMenuController.inst.SetState(currentMode.showTemp &&
                                                 SimulationModeManager.CurrMode==SimModes.MD);
-        ModeMenuController.inst.SetState(currentMode.showModes);
+        ModeMenuController.inst.SetState(currentMode.mode == Modes.Menu);
         ModeMenuController.inst.OnModeChange();
         AnimationMenuController.inst.SetState(//currentMode.mode == Modes.MD ||
                                               currentMode.mode == Modes.Minimize || currentMode.mode == Modes.Animate);
@@ -128,6 +132,6 @@ public class ModeController : MonoBehaviour
         //StructureCreatorMenuController.inst.SetState(currentMode.showPeriodicSystem);
         //if (currentMode.showPeriodicSystem)
         //    StructureCreatorMenuController.inst.OnModeChange();
-        PeriodicSysMenuController.inst.SetState(currentMode.showPeriodicSystem);
+        PeriodicSysMenuController.inst.SetState(currentMode.mode == Modes.StructureBuilder);
     }
 }
