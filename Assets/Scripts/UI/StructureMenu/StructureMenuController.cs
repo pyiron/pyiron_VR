843eb2edc3e5b7c42ae0cdfff06f01fd70b6629b
Wuselwog <30385654+Wuselwog@users.noreply.github.com>
2020-04-02 22:27:37 +0200

    The Structure Menu is now working, except for the visualization.

--- Assets/Scripts/UI/StructureMenu/StructureMenuController.cs
+++ Assets/Scripts/UI/StructureMenu/StructureMenuController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,62 +8,113 @@ public class StructureMenuController : MenuController
 {
     public static StructureMenuController Inst;
 
+    public Dropdown elementDropdown;
+    public Dropdown repeatDropdown;
+    public Toggle cubicToggle;
+    public Toggle orthorombicToggle;
+    public Text ErrorText;
+
     private void Awake()
     {
         Inst = this;
     }
 
-    private void LoadStructure()
+    private void LoadStructure(string structure)
     {
-        // Load the data
-        string order = "get_data()";
-        string structure = PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.eval_l, order);
         print(structure);
+        if (structure.StartsWith("Error"))
+        {
+            ErrorText.text = structure;
+        }
+        else
+        {
+            ErrorText.text = "";
+            
+            for (int i = 0; i < elementDropdown.options.Count; i++)
+            {
+                if (structure.StartsWith(elementDropdown.options[i].text))
+                {
+                    elementDropdown.value = i;
+                }
+            }
+
+            repeatDropdown.value = 0;
+            
+            // set cubic and orthorhombic to default value? Or maybe load it somehow
+        }
         // feed the data into the ImportStructure script to create the new structure or update it
 //        ImportStructure.inst.
     }
     
     public void OnModeStart()
     {
-        LoadStructure();
+        // Load the data
+        string order = "get_data()";
+        string structure = PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.eval_l, order);
+
+        // visualize the structure
+        LoadStructure(structure);
     }
 
-    public void OnElementChange(Dropdown elementDropdown)
+//    public void OnElementChange(Dropdown elementDropdown)
+//    {
+//        string order = "structure.name = '" + elementDropdown.options[elementDropdown.value].text + "'";
+//        PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.exec_l, order);
+//        LoadStructure();
+//    }
+//    
+//    public void OnRepeatChange(Dropdown repeatDropdown)
+//    {
+//        string val = repeatDropdown.options[repeatDropdown.value].text;
+//        string order = "structure = structureManager.structure.repeat([" + val + ", " + val + ", " + val + "])";
+//        PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.exec_l, order);
+//        LoadStructure();
+//    }
+//    
+//    public void OnCubicChange(Toggle cubicToggle)
+//    {
+//        string isOn = "False";
+//        if (cubicToggle.isOn)
+//        {
+//            isOn = "True";
+//        }
+//        string order = "structure.cubic = " + isOn;
+//        PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.exec_l, order);
+//        LoadStructure();
+//    }
+//    
+//    public void OnOrthorombicChange(Toggle orthorombicToggle)
+//    {
+//        string isOrthorombic = "False";
+//        if (orthorombicToggle.isOn)
+//        {
+//            isOrthorombic = "True";
+//        }
+//        string order = "structure.cubic = " + isOrthorombic;
+//        PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.exec_l, order);
+//        LoadStructure();
+//    }
+
+    public void OnStructureChange()
     {
-        string order = "structure.name = '" + elementDropdown.options[elementDropdown.value].text + "'";
-        PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.exec_l, order);
-        LoadStructure();
-    }
-    
-    public void OnRepeatChange(Dropdown repeatDropdown)
-    {
-        string val = repeatDropdown.options[repeatDropdown.value].text;
-        string order = "structure = structureManager.structure.repeat([" + val + ", " + val + ", " + val + "])";
-        PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.exec_l, order);
-        LoadStructure();
-    }
-    
-    public void OnCubicChange(Toggle cubicToggle)
-    {
-        string isOn = "False";
+        string isCubic = "False";
         if (cubicToggle.isOn)
         {
-            isOn = "True";
+            isCubic = "True";
         }
-        string order = "structure.cubic = " + isOn;
-        PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.exec_l, order);
-        LoadStructure();
-    }
-    
-    public void OnOrthorombicChange(Toggle orthorombicToggle)
-    {
+        
         string isOrthorombic = "False";
         if (orthorombicToggle.isOn)
         {
             isOrthorombic = "True";
         }
-        string order = "structure.cubic = " + isOrthorombic;
-        PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.exec_l, order);
-        LoadStructure();
+        
+        string repeat = repeatDropdown.options[repeatDropdown.value].text;
+
+        string element = "'" + elementDropdown.options[elementDropdown.value].text + "'";
+
+        string order = "create(" + element + ", " + repeat + ", " + isCubic + ", " + isOrthorombic + ")";
+        string structure = PythonExecuter.SendOrderSync(PythonScript.StructureManager, PythonCommandType.eval_l, order);
+        LoadStructure(structure);
     }
 }
