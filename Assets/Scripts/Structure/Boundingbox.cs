3751e484016d9d75c33bd9a8bdac8e75aacfcdcd
Wuselwog <30385654+Wuselwog@users.noreply.github.com>
2020-04-04 15:08:09 +0200

    The cell borders are now spheres. This way,the vertices look better. The midpoint of the structure should now be calculated correctly.

--- Assets/Scripts/Structure/Boundingbox.cs
+++ Assets/Scripts/Structure/Boundingbox.cs
@@ -54,37 +54,10 @@ public class Boundingbox : MonoBehaviour
             for (int j = 0; j < 3; j++)
             {
                 Vector3 newSize = Vector3.one * ProgramSettings.cellboxWidth;
-                newSize[2] = data[j].magnitude + ProgramSettings.cellboxWidth;
-//                print(j * 4 + i);
-//                _borders[j * 4 + i].transform.localScale = newSize;
+                newSize[2] = data[j].magnitude;
                 _borders[j + i * 3].transform.localScale = newSize;
-                
-//                Vector3 cellBorderSize = _borders[j * 4 + i].transform.localScale;
-//                cellBorderSize[j] = data[j].magnitude + ProgramSettings.cellboxWidth;
-//                _borders[j * 4 + i].transform.localScale = cellBorderSize;
-
-//                _borders[j * 4 + i].transform.localPosition = data[j] * 0.5f;
-//                if (i == 1 || i == 3)
-//                    _borders[j * 4 + i].transform.localPosition += data[(j + 1) % 3];
-//                if (i == 2 || i == 3)
-//                    _borders[j * 4 + i].transform.localPosition += data[(j + 2) % 3];
             }
         }
-
-//        _borders[0].transform.localPosition = data[0] * 0.5f;
-//        _borders[1].transform.localPosition = data[0] * 0.5f + data[1];
-//        _borders[2].transform.localPosition = data[0] * 0.5f + data[2];
-//        _borders[3].transform.localPosition = data[0] * 0.5f + data[1] + data[2];
-//        
-//        _borders[4].transform.localPosition = data[1] * 0.5f;
-//        _borders[5].transform.localPosition = data[1] * 0.5f + data[0];
-//        _borders[6].transform.localPosition = data[1] * 0.5f + data[2];
-//        _borders[7].transform.localPosition = data[1] * 0.5f + data[0] + data[2];
-//        
-//        _borders[8].transform.localPosition = data[2] * 0.5f;
-//        _borders[9].transform.localPosition = data[2] * 0.5f + data[1];
-//        _borders[10].transform.localPosition = data[2] * 0.5f + data[0];
-//        _borders[11].transform.localPosition = data[2] * 0.5f + data[1] + data[0];
 
         int[] signs = {1, 1, 1, -1, -1, 1, -1, 1, -1, 1, -1, -1};
 
@@ -110,17 +83,9 @@ public class Boundingbox : MonoBehaviour
                 Vector3 newPos = edgePose + data[j] / 2f * signs[i * 3 + j];
                 _borders[i * 3 + j].transform.LookAt(newPos);
                 _borders[i * 3 + j].transform.localPosition = newPos;
-                
-                
-//                Vector3 localPos = _borders[i * 3 + j].transform.localPosition;
-//                localPos[j % 3] += _borders[i * 3 + j].transform.localScale.z / 2f;
-//                _borders[i * 3 + j].transform.localPosition = localPos;
-                
-//                _borders[i * 3 + j].transform.localPosition +=
-//                    Vector3.right * _borders[i * 3 + j].transform.localScale.z;
             }
         }
         
-        mid = new Vector3(data[0][0], data[1][1], data[2][2]);
+        mid = (data[0] + data[1] +  data[2]) / 2f;
     }
 }
