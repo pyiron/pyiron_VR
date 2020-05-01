85974538bdb0984becc20b9ee37096e850d8beea
Wuselwog <30385654+Wuselwog@users.noreply.github.com>
2020-05-01 12:56:43 +0200

    In the structure menu, the button used for choosing the element now has the color of the current element.

--- Assets/Scripts/UI/StructureMenu/PeriodicTable.cs
+++ Assets/Scripts/UI/StructureMenu/PeriodicTable.cs
@@ -12,12 +12,23 @@ public class PeriodicTable : MenuController
         Inst = this;
     }
 
+    private void Start()
+    {
+        SetNewElement("Fe");
+    }
+
+    private void SetNewElement(string element)
+    {
+        StructureMenuController.Inst.elementButton.interactable = true;
+        StructureMenuController.Inst.elementButton.image.color = LocalElementData.GetColour(element);
+        SetState(false);
+    }
+
     public void OnElementButtonDown()
     {
         string element = EventSystem.current.currentSelectedGameObject.name.Split('-')[1];
+        SetNewElement(element);
         StructureMenuController.Inst.SetElementButton(element);
         StructureMenuController.Inst.SetElementDropdown(element);
-        StructureMenuController.Inst.elementButton.interactable = true;
-        SetState(false);
     }
 }
